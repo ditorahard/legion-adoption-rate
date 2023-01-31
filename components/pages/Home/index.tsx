0964ef76ebda9df309813f9fe37c189639badb5f
2nd <the2ndflamingheart@gmail.com>
2023-01-31 08:37:02 +0700

    refactor: refactor Design System Adoption section in Home

--- components/pages/Home/index.tsx
+++ components/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Body, Box, Caption, Card, Flex, Heading } from "legion-ui";
+import { Anchor, Body, Box, Button, Caption, Card, Dropdown, DynamicIcon, Flex, Heading, Select } from "legion-ui";
 import { Bar, Line } from "react-chartjs-2";
 import { BookOpen, Code, Figma, Folder, Logo } from "../../icons";
 import * as usagesByMonth from '../../../data/usages_by_month.json'
@@ -6,6 +6,8 @@ import * as usagesByProject from '../../../data/usages_by_project.json';
 import { BarElement, CategoryScale, Chart as ChartJS, Filler, Legend, LinearScale, LineElement, PointElement, Tooltip } from "chart.js";
 import SummaryInfo from "./SummaryInfo";
 import AssetAdoption from "./AssetAdoption";
+import { useState } from "react";
+import DesignSystemAdoption from "./DesignSystemAdoption";
 
 ChartJS.register(
     CategoryScale,
@@ -17,14 +19,6 @@ ChartJS.register(
     Tooltip,
     Legend
 );
-
-const usagesByMonthOptions = {
-    responsive: true,
-    legend: {
-        position: 'right' as const,
-    },
-};
-
 
 const labels = Object.keys(usagesByMonth).reverse().filter((v => v !== 'default'))
 
@@ -180,7 +174,21 @@ const adoptionEachProjects = [
     },
 ]
 
+type SelectOption = {
+    value: string;
+    label: JSX.Element
+}
+
+const projects = [
+    { value: "all", label: <Body color="tertiary500" size="sm_regular">All Repository Project</Body> },
+    { value: "logee_port", label: <Body color="tertiary500" size="sm_regular">Logee Port</Body> },
+    { value: "logee_order", label: <Body color="tertiary500" size="sm_regular">Logee Order</Body> },
+]
+
+
 export default function Home() {
+    const [selectedProject, setProjectSelector] = useState(projects[0]);
+    const onSetProjectSelector = (selectedProject: SelectOption) => setProjectSelector(selectedProject)
     return <Box bg="secondary25">
         <Flex px={4} bg='black' sx={{ height: '64px', alignItems: 'center' }}>
             <Logo />
@@ -201,33 +209,29 @@ export default function Home() {
             </Flex>
             <Flex mt="24px" sx={{ gap: '24px' }}>
                 <AssetAdoption componentPercentage={0} adoptionEachProjects={adoptionEachProjects} />
-                <Card variant='bordered' p={3} sx={{ width: '75%' }}>
-                    <Box>
-                        <Body size="lg_bold" sx={{ display: 'block' }}>
-                            Design system Adoption
-                        </Body>
-                    </Box>
-                    <Box mb="24px">
-                        <Body size="sm_regular" sx={{ display: 'block' }}>
-                            Design system vs local component usages across all tracked projects
-                        </Body>
-                    </Box>
-                    <Line options={usagesByMonthOptions} data={usagesByMonthChartData} style={{
-                        maxHeight: '328px'
-                    }} />
-                </Card>
+                <DesignSystemAdoption usagesByMonthChartData={usagesByMonthChartData} />
             </Flex>
             <Card mt="24px" variant='bordered' p={3} sx={{ width: '100%' }}>
-                <Box>
-                    <Body size="lg_bold" sx={{ display: 'block' }}>
-                        Component Asset system Adoption
-                    </Body>
-                </Box>
-                <Box>
-                    <Body size="sm_regular" sx={{ display: 'block' }}>
-                        Design system vs local component usages across all tracked projects
-                    </Body>
-                </Box>
+                <Flex>
+                    <Box>
+                        <Box>
+                            <Body size="lg_bold" sx={{ display: 'block' }}>
+                                Component Asset system Adoption
+                            </Body>
+                        </Box>
+                        <Box>
+                            <Body size="sm_regular" sx={{ display: 'block' }}>
+                                Design system vs local component usages across all tracked projects
+                            </Body>
+                        </Box>
+                    </Box>
+                    <Box ml="auto" sx={{ width: '23%' }} >
+                        <Select
+                            options={projects}
+                            value={selectedProject} onChange={v => onSetProjectSelector(v as SelectOption)}
+                        />
+                    </Box>
+                </Flex>
                 <Flex mt={3}>
                     <Box sx={{ width: '50%' }}>
                         <Box>
